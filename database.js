abe813a5265666ae9f06597f7ba3f7a57d19ec15
thayvo <buikhacvinh18@gmail.com>
2024-10-11 10:20:14 +0700

    11/10/24 10:20

---
+++ database.js
@@ -0,0 +1,14 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+const result = await pool.query("select * from customers").then();
+console.log(result);
